6918e394bdb2ba1b5085ae0544f7d333b01cd361
dailyco <dailyco03@gmail.com>
2021-05-07 21:09:32 +0900

    [완료] 프로그래머스.완주하지_못한_선수 #90

---
+++ 프로그래머스/코딩테스트_고득점_kit/해시/완주하지_못한_선수/dailyco.js
@@ -0,0 +1,20 @@
+/*
+작성자 : dailyco
+문제 난이도: 하
+해결시간: 00:10:00
+문제 링크 : https://programmers.co.kr/learn/courses/30/lessons/42576
+블로그 링크 : https://dailyco.github.io/posts/uncompleted-player/
+*/
+
+function solution(participant, completion) {
+  var s = completion.length;
+
+  participant.sort();
+  completion.sort();
+
+  for (var i = 0; i < s; i++) {
+    if (participant[i] != completion[i]) return participant[i];
+  }
+
+  return participant[s];
+}
